a37537154715ee3b3727ea7c1cebb02a0ebfed31
Adewole Adebayo <wole1105@gmail.com>
2022-06-14 22:26:33 -0700

    Assignment completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('form').addEventListener('submit' , (e) => {
+  e.preventDefault()
+  createToDo(e.target['new-task-description'].value)
+  })
 });
+
+function createToDo (toDoList) {
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener("click" , deleteBtn)
+  btn.textContent = 'x'
+  p.textContent = toDoList
+  p.appendChild(btn)
+  document.querySelector('#list').appendChild(p)
+  document.querySelector('form').reset()
+  
+}
+
+function deleteBtn(e) {
+  e.target.parentNode.remove()
+}
